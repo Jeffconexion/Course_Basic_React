69f6aae5151ce91627ed007f1deaaecbf6df1cf0
Jefferson <jeff.debian2016@gmail.com>
2023-02-24 07:31:06 -0300

    Criation structure api and database.

---
+++ Alunos.Api/Context/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Alunos.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alunos.Api.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> op) : base(op)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().HasData(
+                    new Aluno
+                    {
+                        Id = 1,
+                        Nome = "Maria da Penha",
+                        Email = "mariapenha@yahoo.com",
+                        Idade = 23
+                    },
+                    new Aluno
+                    {
+                        Id = 2,
+                        Nome = "Manuel da Penha",
+                        Email = "manu@yahoo.com",
+                        Idade = 25
+                    }
+               );
+        }
+    }
+}
---
+++ Alunos.Api/Migrations/20230224102249_Inicial.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Alunos.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alunos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alunos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "mariapenha@yahoo.com", 23, "Maria da Penha" },
+                    { 2, "manu@yahoo.com", 25, "Manuel da Penha" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alunos");
+        }
+    }
+}
