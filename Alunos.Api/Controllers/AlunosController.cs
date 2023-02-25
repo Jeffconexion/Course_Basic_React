daeda7a284173c486b449f2480323e0f856c71fb
Jefferson <jeff.debian2016@gmail.com>
2023-02-25 14:18:42 -0300

    Todos os enpoints da api criadas com sucesso!

--- Alunos.Api/Controllers/AlunosController.cs
+++ Alunos.Api/Controllers/AlunosController.cs
@@ -1,9 +1,10 @@
-﻿using Alunos.Api.Services.Interfaces;
+﻿using Alunos.Api.Models;
+using Alunos.Api.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Alunos.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/alunos")]
     [ApiController]
     public class AlunosController : ControllerBase
     {
@@ -14,6 +15,73 @@ namespace Alunos.Api.Controllers
             _alunoServices = alunoServices;
         }
 
+        [HttpGet]
+        [Route("obter")]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunos()
+        {
+            var alunos = await _alunoServices.GetAlunos();
+            return Ok(alunos);
+        }
+
+
+        [HttpGet]
+        [Route("obter/nome")]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunosByName([FromQuery]string nome)
+        {
+            var alunos = await _alunoServices.GetAlunoByName(nome);
+            return Ok(alunos);
+        }
+
+        [HttpGet]
+        [ActionName("GetAluno")]
+        [Route("obter/{id:int}", Name = "GetAluno")]
+        public async Task<ActionResult<Aluno>> GetAluno(int id)
+        {
+            var alunos = await _alunoServices.GetAluno(id);
+            return Ok(alunos);
+        }
+
+        [HttpPost]
+        [Route("criar")]
+        public async Task<ActionResult> Create([FromBody] Aluno aluno)
+        {
+            await _alunoServices.CreateAluno(aluno);
+            return CreatedAtRoute(nameof(GetAluno), new {id = aluno.Id}, aluno);           
+        }
+
+        [HttpPut]
+        [Route("edit/{id:int}")]
+        public async Task<ActionResult> Edit(int id, [FromBody] Aluno aluno)
+        {
+
+            if(id == aluno.Id)
+            {
+                await _alunoServices.UpadateAluno(aluno);
+                return Ok($"Aluno com id={id} foi atualizado com sucesso.");
+            }
+            else
+            {
+                return BadRequest("Dados inconsistentes.");
+            }
+        }
+
+        [HttpDelete]
+        [Route("delete/{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            Aluno aluno = await _alunoServices.GetAluno(id);
+
+            if(aluno is not null)
+            {
+               await _alunoServices.DeleteAluno(aluno);
+                return Ok($"Aluno com id={id} foi excluído com sucesso.");
+            }
+            else
+            {
+                return NotFound($"Aluno com id={id} não ncontrado.");
+
+            }
+        }
 
     }
 }
